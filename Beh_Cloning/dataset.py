31d3c0107ff1b4d5f0af4c792a3c7927c30987ef
karthikbmk <karthikbm2007@gmail.com>
2019-08-09 03:41:14 +0530

    add generator

--- Beh_Cloning/dataset.py
+++ Beh_Cloning/dataset.py
@@ -1,33 +1,58 @@
 from helper import Helper
 import numpy as np
 import cv2
+from sklearn.utils import  shuffle
+from sklearn.model_selection import train_test_split
 
 class Dataset:
     def __init__(self, path = '/Users/karthik/Desktop/beta_simulator_mac/Data/driving_log.csv'):
         self.path = path
+        self.root_path = self.path.split('/')
         self.helper = Helper()
+        self.augmentors = {'flip'}
+        self.aug_factor = len([self.augmentors])
 
-    def get_X_y(self):
+    def get_generator(self, samples, batch_size=32, gen_type= 'train_gen'):
+        '''
+        :param samples: A list of dictionaries containing the csv dataset.
+        :param batch_size: Size of the batch
+        :return: A generator, that yeilds a batch of size batch_size * aug_factor from samples.
+        '''
+        num_samples = len(samples)
+        while 1:  # Loop forever so the generator never terminates
+            shuffle(samples)
+            for offset in range(0, num_samples, batch_size):
+                batch_samples = samples[offset:offset + batch_size]
 
+                images = []
+                angles = []
+                for batch_sample in batch_samples:
+                    center_img_name = self.root_path + batch_sample['center'].split('/')[-1]
+                    center_img = cv2.imread(center_img_name)
+                    center_img = cv2.cvtColor(center_img, cv2.COLOR_BGR2RGB)
 
-        images = []
-        steering_angles = []
-        lines = self.helper.csv_to_list(self.path)
+                    center_angle = float(batch_sample['angle'])
+                    images.append(center_img)
+                    angles.append(center_angle)
 
-        for line in lines:
-            center_img_path = line['center']
-            image = cv2.imread(center_img_path)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            images.append(image)
+                    if gen_type == 'train_gen':
+                        if 'flip' in self.augmentors:
+                            flipped_center_img = self.flipper(center_img)
+                            flipped_center_angle = -center_angle
 
-            steering_angle = float(line['angle'])
-            steering_angles.append(steering_angle)
+                            images.append(flipped_center_img)
+                            angles.append(flipped_center_angle)
+                            
+                # trim image to only see section with road
+                X_train = np.array(images)
+                y_train = np.array(angles)
 
-        X = np.array(images)
-        y = np.array(steering_angles)
+                yield shuffle(X_train, y_train)
 
+    def flipper(self, img):
+        rev_img = cv2.flip(img, 1)
+        return  rev_img
 
-        return X, y
 
 '''
 dataset = Dataset()
